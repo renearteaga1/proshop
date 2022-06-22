012798bb2625670d1beaf9b53407029c7f96cb91
Rene Arteaga <rene@renes-imac.lan>
2022-06-22 08:52:13 -0500

    First commit

---
+++ backend/base/products.py
@@ -0,0 +1,26 @@
+products = [
+    {
+        '_id': "1",
+        'name': "Airpods",
+        'image': "",
+        'description': "Bluetooth technology",
+        'brand': "Apple",
+        'category': "Electronics",
+        'price': 89.99,
+        'countInStock': 0,
+        'rating': 4.5,
+        'numReviews': 12,
+    },
+    {
+        '_id': "2",
+        'name': "Computer",
+        'image': "",
+        'description': "Super quantum computer",
+        'brand': "Apple",
+        'category': "Electronics",
+        'price': 1089.99,
+        'countInStock': 3,
+        'rating': 4.9,
+        'numReviews': 3,
+    },
+]
---
+++ backend/base/views.py
@@ -0,0 +1,50 @@
+import imp
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+# from .products import products
+
+# Create your views here.
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+
+        '/api/products/upload/',
+
+        '/api/products/<id>/reviews',
+
+        '/api/products/top/',
+        '/api/producst/<id>',
+
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/',
+    ]
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    # product = None
+    # for i in products:
+    #     if i['_id'] == pk:
+    #         product = i
+    #         break
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
