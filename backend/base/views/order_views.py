288e79e9151893e290917344216e96acb506a81a
Rene <renev88@gmail.com>
2022-07-19 13:05:43 -0500

    Paypal and paypal buttons

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 
 from rest_framework.decorators import api_view, permission_classes
@@ -71,4 +72,14 @@ def getOrderById(request, pk):
         else:
             return Response({'detail': 'Not authorized to view this order'}, status=status.HTTP_400_BAD_REQUEST)
     except:
-        return Response({'detail': 'Order does not exists'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request, pk):
+    order = Order.objects.get(_id=pk)
+    
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+    return Response('Order was paid')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreens from "./screens/ProfileScreens";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import OrderScreen from "./screens/orderScreen";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="/shipping" component={ShippingScreen} />
           <Route path="/payment" component={PaymentScreen} />
           <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/order/:id" component={OrderScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
         </Container>
